800d3afca6be8b7892a228fddbf8e7a7a98eb8d0
Katie Blizzard <kblizzard11@gmail.com>
2019-03-23 20:53:45 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,10 @@
 class Backer 
   
-  attr_reader :backed_projects
+  attr_reader :backed_projects, :name 
   
-  def initialize
+  def initialize(name)
     @backed_projects = []
+    @name = name 
   end 
   
   def back_project(project)
